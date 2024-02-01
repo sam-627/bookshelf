475551658774e3dc241931c8b797ad656b9200df
Samarthya Singh <samarthya.singh@sprinklr.com>
2024-02-01 16:33:21 +0530

    exercise 04: extra credits

--- src/app.exercise.js
+++ src/app.exercise.js
@@ -1,35 +1,47 @@
 /** @jsx jsx */
-import {jsx} from '@emotion/core'
+import { jsx } from '@emotion/core'
 
 import * as React from 'react'
-// 🐨 you're going to need this:
 import * as auth from 'auth-provider'
-import {AuthenticatedApp} from './authenticated-app'
-import {UnauthenticatedApp} from './unauthenticated-app'
+import { AuthenticatedApp } from './authenticated-app'
+import { UnauthenticatedApp } from './unauthenticated-app'
+import { client } from 'utils/api-client.exercise'
 
 function App() {
-  // 🐨 useState for the user
-  const [user, setUser] = React.useState(null);
+  const [user, setUser] = React.useState(null)
 
-  // 🐨 create a login function that calls auth.login then sets the user
-  // 💰 const login = form => auth.login(form).then(u => setUser(u))
-  const login = formData => auth.login(formData).then(user => setUser(user));
-  // 🐨 create a registration function that does the same as login except for register
-  const register = formData => auth.register(formData).then(user => setUser(user));
+  const login = form => auth.login(form).then(u => setUser(u))
+  const register = form => auth.register(form).then(u => setUser(u))
+  const logout = () => {
+    auth.logout()
+    setUser(null)
+  }
 
-  // 🐨 create a logout function that calls auth.logout() and sets the user to null
-  const logout = () => auth.logout().then(() => setUser(null));
 
-  // 🐨 if there's a user, then render the AuthenticatedApp with the user and logout
-  // 🐨 if there's not a user, then render the UnauthenticatedApp with login and register
-  if(user) return <AuthenticatedApp user={user} logout={logout} />
+  const checkToken = async () => {
+    const token = await auth.getToken()
+    console.log({ token })
+    if (token) {
+      // we're logged in! Let's go get the user's data:
+      client('me', { token }).then(data => { 
+        console.log(data.user)
+        setUser(data.user)
+      }).catch(console.log)
+    } else {
+      // we're not logged in. Show the login screen
+      setUser(null);
+    }
+  }
 
-  return <UnauthenticatedApp login={login} register={register} />
+  React.useEffect(() => {
+    checkToken();
+  }, [])
+
+  return user ? (
+    <AuthenticatedApp user={user} logout={logout} />
+  ) : (
+    <UnauthenticatedApp login={login} register={register} />
+  )
 }
 
-export {App}
-
-/*
-eslint
-  no-unused-vars: "off",
-*/
+export { App }
