413d07e1f6feb62053e7b4b726a66130f43912a6
Samarthya Singh <samarthya.singh@sprinklr.com>
2024-02-05 15:35:28 +0530

    completed performance module

--- src/app.exercise.js
+++ src/app.exercise.js
@@ -1,16 +1,21 @@
 import * as React from 'react'
-import {useAuth} from './context/auth-context'
+import { useAuth } from './context/auth-context'
 // 🐨 you'll want to render the FullPageSpinner as the fallback
-// import {FullPageSpinner} from './components/lib'
 
 // 🐨 exchange these for React.lazy calls
-import {AuthenticatedApp} from './authenticated-app'
-import {UnauthenticatedApp} from './unauthenticated-app'
+// import AuthenticatedApp from './authenticated-app'
+// import UnauthenticatedApp from './unauthenticated-app'
+import { FullPageSpinner } from 'components/lib'
+
+const UnauthenticatedApp = React.lazy(() => import('./unauthenticated-app'))
+const AuthenticatedApp = React.lazy(() => import(/* webpackPrefetch: true */'./authenticated-app'))
 
 function App() {
-  const {user} = useAuth()
-  // 🐨 wrap this in a <React.Suspense /> component
-  return user ? <AuthenticatedApp /> : <UnauthenticatedApp />
+  const { user } = useAuth()
+
+  return (<React.Suspense fallback={<FullPageSpinner />}>
+    {user ? <AuthenticatedApp /> : <UnauthenticatedApp />}
+  </React.Suspense>)
 }
 
-export {App}
+export { App }
